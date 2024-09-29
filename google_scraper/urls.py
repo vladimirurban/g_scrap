92e9f75f79120e986ee2daf7e523ec4607f92cc5
vladimirurban <134784936+vladimirurban@users.noreply.github.com>
2024-09-29 17:24:24 +0200

    Update urls.py

--- google_scraper/urls.py
+++ google_scraper/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from search import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('download_csv/<str:keyword>/', views.download_csv, name='download_csv'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
